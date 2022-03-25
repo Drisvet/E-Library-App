be361da170c1856e3dc3bc60d10927cff6207ba4
Drisvet <71675411+Drisvet@users.noreply.github.com>
2022-03-26 03:17:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import BottomTabNavigator from './Component/bottomTab';
+
+export default class App extends Component {
+  render(){
+    return <BottomTabNavigator/>
+  }
+}
+
